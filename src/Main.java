11a5a8298272035090d9e9e2dcd3c1261562cdf3
selden39 <selden39@gmail.com>
2023-12-11 16:54:50 +0300

    second version (with MultiThreading)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
-        String folderPath = "C:\\SkillBox_GitHub\\SkillBox\\";
+
+        String folderPath = "D:\\Temp\\";
         File file = new File(folderPath);
+
+        //однопоточно
+        long start1 = System.currentTimeMillis();
         System.out.println(getFolderSize(file));
+        long duration1 = (System.currentTimeMillis() - start1);
+        System.out.println(duration1 + "ms\n");
+
+        //многопоточно
+        long start = System.currentTimeMillis();
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+        long duration = (System.currentTimeMillis() - start);
+        System.out.println(duration + "ms");
     }
 
     public static long getFolderSize(File folder){
